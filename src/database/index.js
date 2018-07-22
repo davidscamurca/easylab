877d79ce8ee093c8e6db7d208e73b08f666a7f8b
David Camurça <david.camurcaa@gmail.com>
2018-07-22 17:38:20 -0400

    update mongoose connection
    Signed-off-by: David Camurça <david.camurcaa@gmail.com>

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 //mongoose.connect('mongodb://localhost/easylabapi');
-mongoose.connect('mongodb://heroku_dzffswpj:heroku_dzffswpj@ds243441.mlab.com:43441/heroku_dzffswpj');
+mongoose.connect('mongodb://<heroku_dzffswpj>:<heroku_dzffswpj>@ds243441.mlab.com:43441/heroku_dzffswpj');
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
