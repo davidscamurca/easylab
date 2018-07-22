8d5943586fd29cd66f50be0df7f5f84890cf4aff
David Camurça <david.camurcaa@gmail.com>
2018-07-22 17:51:29 -0400

    update mongoose connection new user
    Signed-off-by: David Camurça <david.camurcaa@gmail.com>

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 //mongoose.connect('mongodb://localhost/easylabapi');
-mongoose.connect('mongodb://<heroku_dzffswpj>:<>@ds243441.mlab.com:43441/heroku_dzffswpj');
+mongoose.connect('mongodb://<davidscamurca2018>:<davidscamurca2018>@ds243441.mlab.com:43441/heroku_dzffswpj');
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
