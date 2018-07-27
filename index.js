59af61c40b762db7d79988769e47f6ab03d459ac
David Camurça <david.camurcaa@gmail.com>
2018-07-26 21:30:17 -0400

    permissoes auth 3
    Signed-off-by: David Camurça <david.camurcaa@gmail.com>

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
-require('./app/controllers/index')(app);
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+});
+
+  require('./app/controllers/index')(app);
 
 var porta = process.env.PORT || 8080;
 app.listen(porta);
