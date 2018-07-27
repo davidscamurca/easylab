6a3633918a3ef64bfa4e04d3c1cfa62dfe2f1227
David Camurça <david.camurcaa@gmail.com>
2018-07-26 21:44:17 -0400

    add teste allow
    Signed-off-by: David Camurça <david.camurcaa@gmail.com>

--- index.js
+++ index.js
@@ -7,14 +7,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
 
-require('./app/controllers/index')(app);
-
-
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+
+require('./app/controllers/index')(app);
 
 var porta = process.env.PORT || 8080;
 app.listen(porta);
