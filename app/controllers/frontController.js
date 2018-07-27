f63a8949f388ac13305133e1338a540cfd5c33c3
David Camurça <david.camurcaa@gmail.com>
2018-07-26 21:35:09 -0400

    permissoes indexjs 4
    Signed-off-by: David Camurça <david.camurcaa@gmail.com>

--- app/controllers/frontController.js
+++ app/controllers/frontController.js
@@ -17,9 +17,4 @@ router.get('/', async (req, res) => {
     }
 });
 
-module.exports = app => app.use('/easy/api/v1', router, function(res, next){
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-    next();
-});
+module.exports = app => app.use('/easy/api/v1', router);
--- index.js
+++ index.js
@@ -6,17 +6,15 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
+
+require('./app/controllers/index')(app);
+
+
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-   next();
+    next();
 });
-
-  require('./app/controllers/index')(app);
 
 var porta = process.env.PORT || 8080;
 app.listen(porta);
-
-//app.listen(3000);
-
-//console.log('Server run on: localhost:3000');
